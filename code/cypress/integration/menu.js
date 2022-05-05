b67627647721c84ddcc462e38d2e7dd4606d6436
EC2 Default User <ec2-user@ip-10-0-1-87.us-west-1.compute.internal>
2022-05-05 13:22:11 +0000

    Changing name of file for header to menu

---
+++ code/cypress/integration/menu.js
@@ -0,0 +1,49 @@
+describe('Menu', () =>{
+    beforeEach(() => {
+        cy.visit('/'); 
+        cy.get('[data-cy=hamburgerMenu]').should('not.exist');
+        cy.get('[data-cy=openHamburgerMenu]').should('be.visible').click();
+    });
+    
+    it('Menu opens and closes when clicking close', () =>{
+        cy.get('[data-cy=hamburgerMenu]').should('be.visible');
+        cy.get('[data-cy=closeHamburgerMenu]').should('be.visible').click();
+        cy.get('[data-cy=hamburgerMenu]').should('not.exist');
+    });
+    
+    it('Menu opens and closes when clicking "Home"', () =>{
+        cy.get('[data-cy=hamburgerMenu]').should('be.visible');
+        cy.get('[data-cy=hamburgerMenuHome]').should('be.visible').click();
+        cy.get('[data-cy=hamburgerMenu]').should('not.exist');
+    });
+    
+    it('Clicking "Home" in menu takes you to menu', () =>{
+        cy.get('[data-cy=hamburgerMenu]').should('be.visible');
+        cy.get('[data-cy=hamburgerMenuHome]').should('be.visible').click();
+        cy.location('pathname').should('eq', '/');
+    });
+    
+    it('Menu opens and closes when clicking "For Agents"', () =>{
+        cy.get('[data-cy=hamburgerMenu]').should('be.visible');
+        cy.get('[data-cy=hamburgerMenuForAgents]').should('be.visible').click();
+        cy.get('[data-cy=hamburgerMenu]').should('not.exist');
+    })
+    
+    it('Clicking "For Agents" in menu takes you to "/tria"', () =>{
+        cy.get('[data-cy=hamburgerMenu]').should('be.visible');
+        cy.get('[data-cy=hamburgerMenuForAgents]').should('be.visible').click();
+        cy.location('pathname').should('eq', '/trial');
+    });
+    
+    it('Menu opens and closes when clicking "Contact us"', () =>{
+        cy.get('[data-cy=hamburgerMenu]').should('be.visible');
+        cy.get('[data-cy=hamburgerMenuContatUs]').should('be.visible').click();
+        cy.get('[data-cy=hamburgerMenu]').should('not.exist');
+    })
+    
+    it('Clicking "For Agents" in menu takes you to "/tria"', () =>{
+        cy.get('[data-cy=hamburgerMenu]').should('be.visible');
+        cy.get('[data-cy=hamburgerMenuContatUs]').should('be.visible').click();
+        cy.location('pathname').should('eq', '/contact');
+    });
+})
