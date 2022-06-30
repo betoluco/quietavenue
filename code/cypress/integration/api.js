62458977d144cf42167530016c9a6739bc227700
EC2 Default User <ec2-user@ip-172-31-12-194.us-west-1.compute.internal>
2022-06-30 02:43:47 +0000

    Updating Cypress 9.7.0

--- code/cypress/integration/api.js
+++ code/cypress/integration/api.js
@@ -15,21 +15,21 @@ describe('API', () =>{
                 expect(response.body[1].estateSuggest[1]).to.eq('Flying Fish St')
                 expect(response.body[1].lotArea).to.eq(2360)
                 expect(response.body[1].price).to.eq(2356000)
-                expect(response.body[1].profilePicture).to.eq('assets/1023-Flying-Fish-St-94404/Front.jpg')
+                expect(response.body[1].profilePicture).to.contain('assets/1023-Flying-Fish-St-94404/Front.jpg')
                 expect(response.body[1].soundScore).to.eq(92)
                 expect(response.body[1].videoLink).to.eq('https://player.vimeo.com/video/421606419?badge=0&autopause=0&player_id=0&app_id=58479')
                 expect(response.body[1].zipCode).to.eq('94404')
                 expect(response.body[1].zipCodeSuggest[0]).to.eq('94404')
                 expect(response.body[1].graphData[0].time).to.eq('2020-08-13T15:42:00')
                 expect(response.body[1].graphData[0].maxLoudness).to.eq('1.0')
-                expect(response.body[1].graphData[0].mp3Link).to.eq('assets/1023-Flying-Fish-St-94404/audioFiles/2020-08-13_15-42-00.mp3')
+                expect(response.body[1].graphData[0].mp3Link).to.contain('assets/1023-Flying-Fish-St-94404/audioFiles/2020-08-13_15-42-00.mp3')
             }
         )
     });
     // it('API blocks incomplete objects', () =>{
     //     cy.request('/api/estates').then((resp) => {
     //         expect(resp.status).to.eq(200)
-    //         expect(resp.body).should('not.include', 'no-address1-estate')
+    //         expect(resp.body).to.deep.equal('id', '2141-Mills-Ave-Menlo-Park-CA-94025')
     //     });
     // });
 });
