7c9da54f5a8b3c4b18558235bd6c2023b8ab0474
EC2 Default User <ec2-user@ip-172-31-12-194.us-west-1.compute.internal>
2023-01-07 18:49:00 +0000

    Cypress test for Card component

--- code/src/estates/Card.js
+++ code/src/estates/Card.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
 
 
 const Card = props =>{
   const link = "/estate/" + props.estate.id;
   return (
-    <div className="shadow-md border-b-2 border-green-600 rounded-md" data-cy={props.estate.id}>
+    <div className="shadow-md border-b-2 border-green-600 rounded-md">
       <Link to={link}>
         <img 
+        data-cy="profilePicture"
         className="w-full h-64 object-cover rounded-md" 
         src={props.estate.profilePicture} 
         alt="Estate" />
         <div key={props.estate.id} className="m-2 ml-6">
-          <h2 className="text-lg text-stone-800 font-semibold">
+          <h2 
+          data-cy="street"
+          className="text-lg text-stone-800 font-semibold">
             {props.estate.address1}
           </h2>
-          <h3 className="text-stone-500">
+          <h3 data-cy="city" className="text-stone-500">
             {props.estate.address2}
           </h3>
         </div>
