21ee98bc9fc1eb21e9587e1f8021b9d417701628
EC2 Default User <ec2-user@ip-172-31-12-194.us-west-1.compute.internal>
2023-02-07 15:43:47 +0000

    Fixing bug in css for video

--- code/src/estate/Vimeo.js
+++ code/src/estate/Vimeo.js
@@ -3,12 +3,13 @@ import React from "react";
 const Vimeo = props => {
   return(
     <div className="mb-4 flex justify-center"  data-cy="estateVideo">
-      <div className= "relative w-full p-56.2 lg:w-9/12 lg:p-42.1 xl:w-6/12  xl:p-28.1">
+      <div className= "relative w-full pb-[56.25%] lg:w-9/12 lg:pb-[42.1%] xl:w-6/12 xl:pb-[28.1%]">
         <iframe 
           src={props.iframeSRC} 
           frameBorder="0"
           className="border border-stone-400 mb-8"
           allow="autoplay; fullscreen; picture-in-picture" 
+          allowFullScreen
           style={{position:"absolute", top:"0", left:"0", width: "100%", height:"100%"}}
           title="House video">
         </iframe>
