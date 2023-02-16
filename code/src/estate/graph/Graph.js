c27a5fee7c72f98374a442ac553324fff8a149b5
EC2 Default User <ec2-user@ip-172-31-12-194.us-west-1.compute.internal>
2023-02-16 23:59:56 +0000

    Adding Y axis

--- code/src/estate/graph/Graph.js
+++ code/src/estate/graph/Graph.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
 import {scaleTime, scaleLinear} from "d3-scale";
 
-
 const Graph = props =>{
-  
   const canvasGraphRef = useRef(null);
+  const canvasAxisRef = useRef(null);
   const canvasBackgroundRef = useRef(null);
   let devicePixelRatio =  1;
   const today  = new Date(props.day);
@@ -13,6 +12,9 @@ const Graph = props =>{
   let loudnessScale = scaleLinear().domain([0, 1]);
   const margin = {bottom: 15, top: 0, left: 12, rigth: 12};
   const playingMinuteYOffset = 12;
+  const YAxisOffset = 3
+  const middleAxisYPosition = 0.2
+  const strongAxisYPosition = 0.5
   const fontSize = '0.75rem sans-serif';
   
   const transformDateToToday = (today, date) =>{
@@ -32,17 +34,6 @@ const Graph = props =>{
     
     timeScale.range([ 0 + margin.rigth, canvasBackground.width - margin.left]);
     loudnessScale.range([canvasBackground.height - margin.bottom , 0]);
-    
-    //x axis
-    const ticks = timeScale.ticks();
-    ctx.font = fontSize;
-    ctx.textAlign = "center";
-    for ( let i = 0; i < ticks.length; i++){
-      ctx.fillText(
-        ticks[i].getHours() + ":00", 
-        timeScale(ticks[i]), 
-        canvasBackground.height);
-    }
     
     // Day background
     const sunrise = transformDateToToday(today, props.sunrise);
@@ -65,14 +56,55 @@ const Graph = props =>{
     ctx.fillRect(10, 0, night, 30);
     ctx.fillRect(timeScale(sunset), 0, night, loudnessScale(0));
   }, []);
+  
+  useEffect(() =>{
+    const canvasAxis = canvasAxisRef.current;
+    const ctx = canvasAxis.getContext('2d');
+    canvasAxis.width = canvasAxis.clientWidth * devicePixelRatio;
+    canvasAxis.height = canvasAxis.clientHeight * devicePixelRatio;
     
+    timeScale.range([ 0 + margin.rigth, canvasAxis.width - margin.left]);
+    loudnessScale.range([canvasAxis.height - margin.bottom , 0]);
+    
+    //X axis
+    const XAxisTicks = timeScale.ticks();
+    ctx.font = fontSize;
+    ctx.textAlign = "center";
+    for ( let i = 0; i < XAxisTicks.length; i++){
+      ctx.fillText(
+        XAxisTicks[i].getHours() + ":00", 
+        timeScale(XAxisTicks[i]), 
+        canvasAxis.height);
+    }
+    
+    //Y axis
+    ctx.beginPath();
+    ctx.moveTo(0, loudnessScale(middleAxisYPosition));
+    ctx.lineTo(canvasAxis.width, loudnessScale(middleAxisYPosition));
+    ctx.strokeStyle = "#d6d3d1";
+    ctx.stroke();
+    ctx.textAlign = "left";
+    ctx.textBaseline = "bottom";
+    ctx.fillText("Middle", YAxisOffset, loudnessScale(middleAxisYPosition));
+    
+    ctx.beginPath();
+    ctx.moveTo(0, loudnessScale(strongAxisYPosition));
+    ctx.lineTo(canvasAxis.width, loudnessScale(strongAxisYPosition));
+    ctx.strokeStyle = "#d6d3d1";
+    ctx.stroke();
+    ctx.textAlign = "left";
+    ctx.textBaseline = "bottom";
+    ctx.fillText("Strong", YAxisOffset, loudnessScale(strongAxisYPosition));
+    
+  }, []);
+  
   useEffect(() =>{
     const canvasGraph = canvasGraphRef.current;
     const ctx = canvasGraph.getContext('2d');
     const devicePixelRatio = window.devicePixelRatio || 1;
     canvasGraph.width = canvasGraph.clientWidth * devicePixelRatio;
     canvasGraph.height = canvasGraph.clientHeight * devicePixelRatio;
-    ctx.clearRect(0,0, canvasGraph.width, canvasGraph.height)
+    ctx.clearRect(0,0, canvasGraph.width, canvasGraph.height);
     
     timeScale.range([ 0 + margin.rigth, canvasGraph.width - margin.left]);
     loudnessScale.range([canvasGraph.height - margin.bottom , 0]);
@@ -111,7 +143,8 @@ const Graph = props =>{
   return (
     <div className="relative mb-2 flex flex-col items-center w-full h-28">
       <canvas ref={canvasBackgroundRef} className="absolute w-full h-full z-0"/>
-      <canvas ref={canvasGraphRef} className=" absolute w-full h-full z-10"/>
+      <canvas ref={canvasAxisRef} className=" absolute w-full h-full z-10"/>
+      <canvas ref={canvasGraphRef} className=" absolute w-full h-full z-20"/>
     </div>
   );
 };
