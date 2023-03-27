d37ae9be7ed2fe7655f6e99898e116e46706c16a
EC2 Default User <ec2-user@ip-172-31-12-194.us-west-1.compute.internal>
2023-03-27 19:48:54 +0000

    Improvements on style

--- code/src/contact/Contact.js
+++ code/src/contact/Contact.js
@@ -2,24 +2,23 @@ import React, { Fragment } from "react";
 
 import Logo from "../common/Logo";
 import HamburgerMenu from "../common/HamburgerMenu";
-import Slogan from "../common/Slogan";
 import logo from "../common/images/quietavenueLogoOp.svg";
 
 const Contact = (props) =>{
   return(
     <Fragment>
-      <header className="mb-10 pb-8 border-b border-green-600" >
-        <div className="flex flex-row justify-between mx-3 mt-2.5 mb-4">
+      <header className="mb-3" >
+        <div className="flex flex-row justify-between mx-3 mt-2.5 pb-2 mb-2 border-b border-strone-100">
           <Logo />
           <HamburgerMenu/>
         </div>
       </header>
       <div className="flex flex-col items-center p-2.5">
         <img src={logo} alt="Company logo" className="h-24 md:h-24 mb-10" />
-        <h4 className="mb-4 max-w-screen-md text-lg font-medium">
+        <h4 className="mb-4 max-w-screen-md text-lg font-medium text-center">
           Any matter related to this web site, please feel free to contact us
         </h4>
-        <h1 className="mb-6 text-center text-2xl font-bold text-green-600">
+        <h1 className="mb-6 text-center text-2xl font-bold text-stone-800">
           +650-391-6820
         </h1>
         
