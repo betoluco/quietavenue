eb9fb1119bc78fc53d0a06fc33d0f19ab0d933f8
EC2 Default User <ec2-user@ip-172-31-12-194.us-west-1.compute.internal>
2023-03-27 19:48:05 +0000

    Improvments on the style component

--- code/src/freeTrial/FreeTrial.js
+++ code/src/freeTrial/FreeTrial.js
@@ -2,30 +2,31 @@ import React, { Fragment } from "react";
 
 import Logo from "../common/Logo"
 import HamburgerMenu from "../common/HamburgerMenu";
-import Slogan from "../common/Slogan";
-import logo from "../common/images/quietavenueLogoOp.svg";
+import realtor from "./realtor.jpg";
 
 const FreeTrial = (props) =>{
   return(
     <Fragment>
-      <header className="mb-10 pb-8 border-b border-green-600" >
-        <div className="flex flex-row justify-between mx-3 mt-2.5 mb-4">
+      <header className="mb-3" >
+        <div className="flex flex-row justify-between mx-3 mt-2.5 pb-2 mb-2 border-b border-strone-100">
           <Logo />
           <HamburgerMenu/>
         </div>
+        <div className="flex flex-col items-center p-2.5">
+          <img src={realtor} className="aspect-auto border border-strone-100 max-w-screen-md w-full h-auto"/>
+        </div>
       </header>
       <div className="flex flex-col items-center p-2.5">
-        <img src={logo} alt="Company logo" className="h-24 md:h-24 mb-10" />
-        <h4 className="mb-4 max-w-screen-md text-lg">
+        <h4 className="mb-4 max-w-screen-md text-lg text-stone-800">
           When you enroll your property for a free trial,* you provide prospective customers with the information they need to make an informed purchase.
         </h4>
-        <h4 className="mb-6 max-w-screen-md text-lg">
+        <h4 className="mb-6 max-w-screen-md text-lg text-stone-800">
           One of our recording technicians will go to your property and set up discrete recording equipment. A week later the analysis of your property’s data will be available in our website. Prospective buyers can find your property’s information through a google search, or you can provide a direct link in your marketing materials or MLS/Zillow/Redfin listing Call now to set up your listing’s recording.
         </h4>
         <h1 className="mb-6 text-center text-2xl font-bold text-green-600">
           Call or Text to Enroll Free! +650-391-6820
         </h1>
-        <h4 className="mb-10 max-w-screen-md text-lg font-medium">
+        <h4 className="mb-10 max-w-screen-md text-lg font-medium text-stone-800">
           * Free trial limited to one property per agent.
         </h4>
       </div>
