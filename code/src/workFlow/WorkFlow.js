2725159e45e252e2e6c179c04396df7f092ca4c1
EC2 Default User <ec2-user@ip-172-31-12-194.us-west-1.compute.internal>
2023-06-08 01:09:54 +0000

    Changes on layout and texts

--- code/src/workFlow/WorkFlow.js
+++ code/src/workFlow/WorkFlow.js
@@ -15,15 +15,22 @@ const WorkFlow = (props) =>{
     <Fragment>
       <h2 
       data-cy=""
-      className="text-2xl sm:text-3xl md:text-4xl xl:text-5xl text-center mb-12 text-stone-800 font-semibold">
+      className="text-2xl sm:text-3xl md:text-4xl xl:text-5xl text-center mb-10 text-stone-800 font-semibold">
         Schedule your <span className="text-green-600" >free trial!</span>
       </h2>
+      
+      <div className="flex justify-center mx-3 mb-10">
+          <p className="text-base text-stone-800 max-w-screen-md">Prospective buyers can find your property’s 
+          information through a google search, or you can provide a direct link in your marketing materials 
+          or MLS/Zillow/Redfin listing Call now to set up your listing’s recording 
+          </p>
+      </div>
       
       <div className="flex justify-center px-3 mb-16 py-16 bg-stone-200">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-screen-lg">
           <div className="flex flex-col bg-white rounded-md px-2 py-2">
             <h3 className="mb-0.5 text-center text-lg text-stone-800 font-semibold">
-              <span className="text-green-600 text-xl">1. </span>Call us!
+              <span className="text-green-600 text-xl">1. </span>Call to scehdule a site visit!
             </h3>
             <img 
             className="mb-2 aspect-auto border border-strone-200 w-full"
@@ -37,7 +44,7 @@ const WorkFlow = (props) =>{
             (min-width: px1024) px341
             100vw"
             src={callUsSM}/>
-            <p className="text-center text-base text-stone-800">Schedule an appointment by calling</p>
+            <p className="text-center text-base text-stone-800">Schedule a visist by calling</p>
             <h2 className="text-center text-2xl text-green-600 font-semibold">
               <Phone />
             </h2>
@@ -92,12 +99,6 @@ const WorkFlow = (props) =>{
           </div>
           
         </div>
-      </div>
-      <div className="flex justify-center mx-3 mb-6">
-        <p className="text-base text-stone-800 max-w-screen-md">Prospective buyers can find your property’s 
-        information through a google search, or you can provide a direct link in your marketing materials 
-        or MLS/Zillow/Redfin listing Call now to set up your listing’s recording 
-        </p>
       </div>
     </Fragment>
   ) ; 
