104ddaffe18d34fa6117e32af135d746ec151e55
EC2 Default User <ec2-user@ip-172-31-17-210.us-west-1.compute.internal>
2023-12-06 16:39:55 +0000

    Modifying code to separate frontend and backend

--- frontEnd/src/app.js
+++ frontEnd/src/app.js
@@ -6,25 +6,11 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import renderer from "./renderer";
 import Routes from "./Routes";
-import estates from "./API/estates";
 import estatesReducer from './estatesReducer';
 
 const app = express();
 
 app.use(awsServerlessExpressMiddleware.eventContext());
-
-// API
-
-const corsDev = `${process.env.CORS}`; //Allow request from Cloud 9 development machine
-
-app.get("/api/estates", cors({origin: corsDev}), async (req, res) =>{
-    const response = await estates(req, res);
-    res.contentType('application/json');
-    res.send(JSON.stringify(response));
-});
-
-//Server side rendering
-
 app.get("*", async (req, res) =>{
     const store = configureStore({
         reducer:{
