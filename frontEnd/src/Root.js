cd46b73e0af9f27912d71fdd5da9eed81a4fd723
EC2 Default User <ec2-user@ip-172-31-17-210.us-west-1.compute.internal>
2023-12-06 18:39:10 +0000

    deleting trie to implement autocomplete using the database

--- frontEnd/src/Root.js
+++ frontEnd/src/Root.js
@@ -1,36 +1,10 @@
 import React, { Fragment, useEffect } from "react";
 import { renderRoutes } from "react-router-config";
-import { useSelector } from "react-redux";
 
-import { estateSuggest } from "./trie";
-import { zipCodeSuggest } from "./trie";
-import { citySuggest } from "./trie";
 import Header from "./common/Header";
 import Footer from "./common/Footer";
 
 const Root = ( props ) => {
-    const estates = useSelector( state => state.estates );
-    
-    useEffect( () => {
-        if (estates.status === 'succeeded'){
-            estates.estates.forEach( estate => {
-                estate.estateSuggest.map( suggest =>{
-                    const name = estate.address1 + " " + estate.address2;
-                    const link = '/estate/'+ estate.id;
-                    estateSuggest.insert(suggest, name, link);
-                });
-                estate.citySuggest.map( suggest =>{
-                    const link = "/?filter=cityId&filterId=" + estate.cityId;
-                    citySuggest.insert(suggest, estate.city, link); 
-                });
-                estate.zipCodeSuggest.map( suggest =>{
-                    const link = "/?filter=zipCode&filterId=" + estate.zipCode;
-                    zipCodeSuggest.insert(suggest, estate.zipCode, link) ;
-                });
-            });
-        }
-    }, [estates.status]);
-    
     useEffect( () => {
         window.gtag('config', 'G-439GZCCJLJ', {
             page_title: props.location.pathname,
