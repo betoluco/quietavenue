3d7b8c8488162a73d75f7ecb4c59c5d5fa0ce0be
Carlos A Hurtado <betoluco@yahoo.com>
2024-04-05 17:34:59 +0000

    Testing Footer

---
+++ frontEnd/src/common/__test__/Footer.test.js
@@ -0,0 +1,13 @@
+import {render, screen} from '@testing-library/react';
+import {BrowserRouter} from 'react-router-dom';
+
+import Footer from '../Footer';
+
+it('Footer home link has href property to home', () =>{
+    render(<Footer/>, {wrapper:BrowserRouter});
+    expect(screen.getByRole('link', {name:'Home'}).href).toMatch(new RegExp('http://[^\/]*/'));
+    expect(screen.getByRole('link', {name:'Schedule your free trial'}).href).toMatch(new RegExp('http://[^\/]*/workFlow'));
+    expect(screen.getByRole('link', {name:'How it works?'}).href).toMatch(new RegExp('http://[^\/]*/mission'));
+    expect(screen.getByRole('link', {name:'FAQ'}).href).toMatch(new RegExp('http://[^\/]*/FAQ'));
+    expect(screen.getByRole('link', {name:'Contact us'}).href).toMatch(new RegExp('http://[^\/]*/'));
+});
