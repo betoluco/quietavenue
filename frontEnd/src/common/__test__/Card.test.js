a0ee2218c3ea69405bd9c5a29ba43ca779f7ec5b
Carlos A Hurtado <betoluco@yahoo.com>
2024-04-05 15:15:28 +0000

    Rearranging imports

---
+++ frontEnd/src/common/__test__/Card.test.js
@@ -0,0 +1,27 @@
+import {render, screen} from '@testing-library/react';
+import {BrowserRouter} from 'react-router-dom';
+
+import CardTemplate from '../CardTemplate';
+
+const estate = {
+    "url": "/estate/2/CA/-Foster-City/2141-Mills-Ave",
+    "profilePicture": "/assets/2141-Mills-Ave-Foster-City-CA-94404/2141-Mills-Ave-Foster-City-CA-94404-main-pic.jpg" ,
+    "address1": "2141 Mills Ave",
+    "address2": "Foster City CA 94044"
+};
+
+it('link should have href', () =>{
+    render(<CardTemplate estate={estate} />, {wrapper:BrowserRouter});
+    expect(screen.getByRole('link').href).toMatch(new RegExp('http://[^\/]*/estate/2/CA/-Foster-City/2141-Mills-Ave'));
+});
+
+it('Image should have src', () =>{
+    render(<Card estate={estate} />, {wrapper:BrowserRouter});
+    expect(screen.getByRole('img').src).toMatch(new RegExp('http://[^\/]*/assets/2141-Mills-Ave-Foster-City-CA-94404/2141-Mills-Ave-Foster-City-CA-94404-main-pic.jpg'));
+});
+
+it('Card componet renders all required information', () =>{
+    render(<Card estate={estate} />, {wrapper:BrowserRouter});
+    expect(screen.getByRole('heading', {name: "2141 Mills Ave"}));
+    expect(screen.getByRole('heading', {name: "Foster City CA 94044"}));
+});
