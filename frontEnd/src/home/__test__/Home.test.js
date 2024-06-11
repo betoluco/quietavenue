998f901f81d6d1b149dc50ca34bf0c866bef41a5
Carlos A Hurtado <betoluco@yahoo.com>
2024-06-11 18:26:53 +0000

    Changint test to use getAllByRole links

--- frontEnd/src/home/__test__/Home.test.js
+++ frontEnd/src/home/__test__/Home.test.js
@@ -44,16 +44,18 @@ it('Home can render cards', async() =>{
   });
   renderWithProviders(Home.element);
   await new Promise(process.nextTick);
-  const link1 = screen.getByLabelText('go to 3843 Bernal Dr Dallas TX 75212');
-  expect(link1).toHaveProperty('href', 'http://localhost:3000/estate/4/TX/-Dallas/3843-Bernal-Dr');
-  expect(within(link1).getByRole('img')).toHaveProperty('src', 'http://localhost:3000/assets/3843-Bernal-Dr-Dallas-TX-75212/3843-Bernal-Dr-Dallas-TX-75212-main-pic.jpg');
-  expect(within(link1).getByRole('heading', {name: "3843 Bernal Dr"})).toBeInTheDocument();
-  expect(within(link1).getByRole('heading', {name: "Dallas TX 75212"})).toBeInTheDocument();
-  const link2 = screen.getByLabelText('go to 83 Mc Kinnie Dr Dallas TX 75234');
-  expect(link2).toHaveProperty('href', 'http://localhost:3000/estate/4/TX/Dallas/83-Mc-Kinnie-Dr');
-  expect(within(link2).getByRole('img')).toHaveProperty('src', 'http://localhost:3000/assets/83-Mc-Kinnie-Dr-Dallas-TX-75234/83-Mc-Kinnie-Dr-Dallas-TX-75234-main-pic.jpg');
-  expect(within(link2).getByRole('heading', {name: "83 Mc Kinnie Dr"})).toBeInTheDocument();
-  expect(within(link2).getByRole('heading', {name: "Dallas TX 75234"})).toBeInTheDocument();
+  const links = screen.getAllByRole('link');
+  expect(links).toHaveLength(4);
+  expect(links[0]).toHaveProperty('href', 'http://localhost:3000/workFlow');
+  expect(links[1]).toHaveProperty('href', 'http://localhost:3000/mission');
+  expect(links[2]).toHaveProperty('href', 'http://localhost:3000/estate/4/TX/-Dallas/3843-Bernal-Dr');
+  expect(within(links[2]).getByRole('img')).toHaveProperty('src', 'http://localhost:3000/assets/3843-Bernal-Dr-Dallas-TX-75212/3843-Bernal-Dr-Dallas-TX-75212-main-pic.jpg');
+  expect(within(links[2]).getByRole('heading', {name: "3843 Bernal Dr"})).toBeInTheDocument();
+  expect(within(links[2]).getByRole('heading', {name: "Dallas TX 75212"})).toBeInTheDocument();
+  expect(links[3]).toHaveProperty('href', 'http://localhost:3000/estate/4/TX/Dallas/83-Mc-Kinnie-Dr');
+  expect(within(links[3]).getByRole('img')).toHaveProperty('src', 'http://localhost:3000/assets/83-Mc-Kinnie-Dr-Dallas-TX-75234/83-Mc-Kinnie-Dr-Dallas-TX-75234-main-pic.jpg');
+  expect(within(links[3]).getByRole('heading', {name: "83 Mc Kinnie Dr"})).toBeInTheDocument();
+  expect(within(links[3]).getByRole('heading', {name: "Dallas TX 75234"})).toBeInTheDocument();
 });
 
 it("Home renders a server error if the request fails", async() =>{
