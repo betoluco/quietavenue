ac93b309875ff8dafaf9009316f18d8798afbde6
Carlos A Hurtado <betoluco@yahoo.com>
2024-06-20 21:34:03 +0000

    Changing the axis ticks

--- frontEnd/src/estate/graph/XAxis.js
+++ frontEnd/src/estate/graph/XAxis.js
@@ -2,20 +2,20 @@ import React, { Fragment } from "react";
 
 const XAxis = props =>{
     const xAxisTicks = [];
-    const shownTicks = [1, 3, 5, 7]
-    props.timeScale.ticks().forEach( (tick, index, array) =>{
-        if (shownTicks.includes(index)) {
-           xAxisTicks.push(
-               <text 
-                key={tick}
-                x={props.timeScale(tick)}
-                y={props.height} 
-                fontSize={`${props.fontSize}rem`}
-                textAnchor="middle">
-                    {tick.getHours() + ":00"}
-                </text>
-            );
-        }
+    props.timeScale.ticks(3).forEach( (tick, index, array) =>{
+        let textAnchor = "middle";
+        if (index === 0) textAnchor = "start";
+        if (index === array.length - 1) textAnchor = "end";
+        xAxisTicks.push(
+            <text 
+            key={tick}
+            x={props.timeScale(tick)}
+            y={props.height} 
+            fontSize={`${props.fontSize}rem`}
+            textAnchor={textAnchor}>
+                {tick.getHours() + ":00"}
+            </text>
+        );
     });
     
     return(
