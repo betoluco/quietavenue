7137b07484ba9bd22a09b3e7374b0eb3bcc194ed
Carlos A Hurtado <betoluco@yahoo.com>
2024-06-27 20:00:37 +0000

    Deleting playerReducer for a refactoring in AudioPlayer

--- frontEnd/src/store.js
+++ frontEnd/src/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import estatesReducer from './estatesReducer';
-import playerReducer from './playerReducer';
 
 export const setupStore = (preloadedState = {}) => {
     return configureStore({
         reducer: {
         estates: estatesReducer,
-        player: playerReducer
         },
         preloadedState
     });
