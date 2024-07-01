e313ece1867dff4d71ce03dcd40ecb04fcc36916
Carlos A Hurtado <betoluco@yahoo.com>
2024-07-01 16:50:30 +0000

    Including config to restore all mocks

--- frontEnd/setupTest.js
+++ frontEnd/setupTest.js
@@ -1,6 +1,7 @@
-import { expect, afterEach } from 'vitest';
+import { expect, afterEach, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 
 afterEach(() => {
   cleanup();
+  vi.restoreAllMocks();
 })
