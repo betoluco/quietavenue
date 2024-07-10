04fcb6c7fdc6a0eef751050ff42a7ac9d73e46e8
Carlos A Hurtado <betoluco@yahoo.com>
2024-07-10 16:57:35 +0000

    Replacing cypress with playwright

--- frontEnd/cypress/integration/card.js
+++
@@ -1,8 +0,0 @@
-describe('Estates cards', () =>{
-    it("Estate cards renders all the properties", () =>{
-        cy.visit('/');
-        cy.get('[data-cy=profilePicture]').should('exist')
-        cy.get('[data-cy=street]').should('exist')
-        cy.get('[data-cy=city]').should('exist')
-    });
-})
