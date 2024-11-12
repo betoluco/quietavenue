38d2fbf8fae99b3115f3c7d71ad9d211d85e6862
Carlos <betoluco@yahoo.com>
2024-11-12 21:17:05 +0000

    Adding a comment

--- e2eTests/databaseScripts/set-read-only-user.sql
+++ e2eTests/databaseScripts/set-read-only-user.sql
@@ -3,5 +3,5 @@ CREATE ROLE readaccess;
 GRANT CONNECT ON DATABASE quietavenue TO readaccess;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO readaccess;
 -- add password DBProxy-password-secret created by secrets manager in the template.yaml
-CREATE USER read_user WITH PASSWORD 'M30Z7BFllfWD7jB5HEiAHQRb28qQy2'; -- the password has to be in single quotes ''
-GRANT readaccess TO read_user;
+CREATE USER read_user WITH PASSWORD 'gmVABGVPOR44JoXn1rXziVq4yKk12R'; -- the password has to be in single quotes ''
+GRANT readaccess TO read_user; -- The user name (read_user) has to be the same used in `samconfig.toml`
